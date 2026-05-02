73bc3396a9d48382846253db5e756d9a22b74eed
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-05-02 16:13:08 +0500

    feat: initialize project structure with core UI component library and Prisma schema

---
+++ lib/auth/auth-client.ts
@@ -0,0 +1,14 @@
+import { polar } from "better-auth";
+import { createAuthClient } from "better-auth/react";
+export const authClient = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL,
+});
+
+export const {
+  signIn,
+  signUp,
+  useSession,
+  changeEmail,
+  resetPassword,
+  signOut,
+} = authClient;
